e05db9ab5e02667d80041ab97663dd8a3f81bf7e
UmeshKumar0143 <149981630+UmeshKumar0143@users.noreply.github.com>
2024-11-16 11:07:58 +0530

    completed basic landing page

---
+++ src/components/TiltText.tsx
@@ -0,0 +1,11 @@
+export default function TiltText(){
+    return <div className="mt-28 ">
+            <h1 className="text-[4vw] font-[anzo3] leading-[4.3vw]">I AM <span className="text-black">DARK MODE</span>™</h1>
+            <h1 className="text-[7.3vw] font-[anzo3] leading-[5.3vw]">DESIGNER</h1>
+            <h1 className="text-[4vw] font-[anzo3] leading-[4.3vw]">TO HIRE</h1>
+    </div>
+}
+
+
+
+
--- src/pages/Page1.tsx
+++ src/pages/Page1.tsx
@@ -1,5 +1,12 @@
+import Page1footer from "../components/Page1footer";
+import TiltText from "../components/TiltText";
+
 export default function Page1(){
-    return <div className="h-screen  px-7 py-5">
-        <div className="w-full h-full shadow-xl shadow-gray-400 bg-cover bg-center bg-[url('https://static.wixstatic.com/media/f1c650_678c0ceab5194893872c60fa3be4bcdc~mv2.jpg/v1/fill/w_1861,h_898,fp_0.69_0.64,q_85,usm_0.66_1.00_0.01,enc_auto/ANZO.jpg')]  rounded-[40px] "></div>
+    return <div className="h-screen  px-5 py-4">
+        <div className="w-full p-20 h-full shadow-xl text-white shadow-gray-400 bg-cover bg-center bg-[url('https://static.wixstatic.com/media/f1c650_678c0ceab5194893872c60fa3be4bcdc~mv2.jpg/v1/fill/w_1861,h_898,fp_0.69_0.64,q_85,usm_0.66_1.00_0.01,enc_auto/ANZO.jpg')]  rounded-[40px] ">
+                <img className="h-20" src="https://static.wixstatic.com/media/f1c650_35a00b1fe6ce403eb237970ec8d34f79~mv2.png/v1/fill/w_99,h_99,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Frame%2053.png" alt="" />
+                <TiltText/>
+                <Page1footer/>
+            </div>
          </div>
 }
